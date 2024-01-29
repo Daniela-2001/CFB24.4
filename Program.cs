e57f6f4e92e7689e2950512c540b8957f503b404
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-29 13:49:00 +0000

    CFB24.4

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace aula24._4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            soma(10,5);
+        }
+
+        static void soma(int n1,int n2)
+        {
+            int res=n1+n2;
+
+            Console.WriteLine("A soma de {0} e {1} é: {2}",n1,n2,res); 
+        }
+    }
+}
